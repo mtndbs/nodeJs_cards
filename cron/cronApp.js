1a343cac5ff75a36a549b2b037e3e2562b4c8bbc
mtndbs <mtndbs@gmail.com>
2023-03-30 00:45:18 +0300

    testing2

--- cron/cronApp.js
+++ cron/cronApp.js
@@ -2,7 +2,7 @@ const { CronJob } = require('cron/lib/cron');
 const { Task } = require('../models/Task');
 const Cyclic = require('../models/CyclicSchema');
 
-const job = new CronJob('0 39 0 30 * *', async () => {
+const job = new CronJob('0 47 0 30 * *', async () => {
   const d = new Date();
   console.log('every first on month:', d);
   const baseCyclic = await Cyclic.find({})
@@ -14,6 +14,7 @@ const job = new CronJob('0 39 0 30 * *', async () => {
     task.month = new Date().getMonth() + 1;
   });
   await Task.insertMany(baseCyclic);
+  console.log('now its REAL');
   console.log(baseCyclic);
 });
 
